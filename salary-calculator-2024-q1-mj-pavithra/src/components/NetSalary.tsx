1ad5f79872174290e227b04b77003daf95350333
mjpavithra <mjpavithra6520@gmail.com>
2024-06-18 15:30:12 +0530

    issue on basic salary is fixed

--- salary-calculator-2024-q1-mj-pavithra/src/components/NetSalary.tsx
+++ salary-calculator-2024-q1-mj-pavithra/src/components/NetSalary.tsx
@@ -1,14 +1,18 @@
 
 
 type NetSalaryProps = {
-    netSalary: number;
+    netSalary: number| null;
   };
 function formatNumberWithCommas(number: number): string {
     return number.toLocaleString('en-US');
 }
   const NetSalary: React.FC<NetSalaryProps> = ({ netSalary }) => {
 
-    const formattedNetSalary = formatNumberWithCommas(netSalary);
+    // const formattedNetSalary = formatNumberWithCommas(netSalary);
+    let formattedNetSalary = '';
+    if (netSalary !== null) {
+      formattedNetSalary = formatNumberWithCommas(netSalary);
+  }
     return (
         <div className="flex justify-between items-center  p-4 rounded-lg mb-4 border border-gray-200 shadow-sm">
             <h2 className="text-lg font-bold">Net Salary (Takehome)</h2>
